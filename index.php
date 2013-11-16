71bb8d94a9d263fe42c5856c373e068209b6fca8
Yang JuneHyuk <nana3993@naver.com>
2013-11-17 03:10:47 +0900

    edit
    edit

--- index.php
+++ index.php
@@ -1,140 +1,373 @@
 <!DOCTYPE html>
 <html>
-  <head>
+<head>
     <meta charset="utf-8">
-    <title>Parse JavaScript Todo App</title>
-    <link href="css/todos.css" media="all" rel="stylesheet" type="text/css"/>
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
-    <script src="js/underscore-1.1.6.js"></script>
+    <title>SSKY STORY</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" >
+    <script src="./js/jquery.min.js"></script>
+    <script src="./js/kendo.mobile.min.js"></script>
+    <link href="./styles/kendo.common.min.css" rel="stylesheet" />
+    <link href="./styles/kendo.mobile.all.min.css" rel="stylesheet" />
     <script src="js/ssky-1.0.js"></script>
-    <script src="js/story.js"></script>
-  </head>
+
+</head>
+<body>
 
 
+<div data-role="view" data-layout="overview-layout" id="overview-cities" data-title="뉴스피드">
+  <ul data-role="listview" data-style="inset" data-type="group">
+        <li>
+            양준혁
+            <ul>
+                <li><h2>Nairobi</h2><img src="../../content/mobile/overview/nairobi.jpg" /></li>
+            </ul>
+        </li>
+        <li>
+            김태훈
+            <ul>
+                <li><h2>Boston</h2><img src="../../content/mobile/overview/boston.jpg" /></li>
+                <li><h2>Ottawa</h2><img src="../../content/mobile/overview/ottawa.jpg" /></li>
+                <li><h2>San Francisco</h2><img src="../../content/mobile/overview/san-francisco.jpg" /></li>
+            </ul>
+        </li>
+        <li>
+            Asia
+            <ul>
+                <li><h2>Bombay</h2><img src="../../content/mobile/overview/bombay.jpg" /></li>
+            </ul>
+        </li>
+        <li>
+            Australia
+            <ul>
+                <li><h2>Melbourne</h2><img src="../../content/mobile/overview/melbourne.jpg" /></li>
+                <li><h2>Sydney</h2><img src="../../content/mobile/overview/sydney.jpg" /></li>
+            </ul>
+        </li>
+        <li>
+            Europe
+            <ul>
+                <li><h2>Cannes</h2><img src="../../content/mobile/overview/cannes.jpg" /></li>
+                <li><h2>Liverpool</h2><img src="../../content/mobile/overview/liverpool.jpg" /></li>
+                <li><h2>London</h2><img src="../../content/mobile/overview/london.jpg" /></li>
+            </ul>
+        </li>
+    </ul>
+</div>
 
-  <body>
+<div data-role="view" data-layout="overview-layout" id="overview-gallery" data-title="내스토리">
+    <div id="scrollview-container">
+        <div data-role="scrollview">
+            <div class="photo photo1"></div>
 
-  <script type="text/javascript">
-    Parse.initialize("YUH91TBS1TBOKVrgDRRxvmUi95luezVVQqNW5j0x", "DltKiTzyzqJ7VJUzGrtfrsKbQoDBX0aU3K3xUHx5");
-    Parse.serverURL = "http://api.ssky.me";
-    
-    function test(){
+            <div class="photo photo2"></div>
 
-      var fileUploadControl = $("#profilePhotoFileUpload")[0];
-      if (fileUploadControl.files.length > 0) {
-        var file = fileUploadControl.files[0];
-        var name = "photo.jpg";
-        var parseFile = new Parse.File(name, file);
-        parseFile.save().then(function(e) {
-          var body = document.getElementsByTagName('body')[0];
-            body.style.backgroundImage = 'url(' + e._url + ')';
-          //alert(e._url);
-        }, function(error) {
-          // The file either could not be read, or could not be saved to Parse.
-        });
-      }
+            <div class="photo photo3"></div>
 
+            <div class="photo photo4"></div>
 
-    }
-  </script>
+            <div class="photo photo5"></div>
 
-  <input type="file" id="profilePhotoFileUpload">
-  <input type="button" onclick="test();"></input>
+            <div class="photo photo6"></div>
 
+            <div class="photo photo7"></div>
 
+            <div class="photo photo8"></div>
 
-    <!-- Todo App Interface -->
+            <div class="photo photo9"></div>
 
-    <div id="todoapp">
-      <div class="title">
-        <h1>Parse Todos</h1>
-      </div>
-
-      <div class="content">
-      </div>
-    </div>
-
-    <div id="credits">
-      Powered by <a href="https://www.parse.com">Parse</a> using the <a href="https://www.parse.com/docs/js_guide">JavaScript SDK</a>.<br /><br />
-      Learn how we built it in the <a href='https://parse.com/tutorials/todo-app-with-javascript'>tutorial</a>.
-    </div>
-
-    <!-- Templates -->
-
-    <script type="text/template" id="login-template">
-      <header id="header"></header>
-      <div class="login">
-        <form class="login-form">
-          <h2>Log In</h2>
-          <div class="error" style="display:none"></div>
-          <input type="text" id="login-username" placeholder="Username" />
-          <input type="password" id="login-password" placeholder="Password" />
-          <button>Log In</button>
-        </form>
-
-        <form class="signup-form">
-          <h2>Sign Up</h2>
-          <div class="error" style="display:none"></div>
-          <input type="text" id="signup-username" placeholder="Username" />
-          <input type="password" id="signup-password" placeholder="Create a Password" />
-          <button>Sign Up</button>
-        </form>
-      </div>
-    </script>
-
-    <script type="text/template" id="manage-todos-template">
-      <div id="user-info">
-        Signed in as <%= Parse.User.current().get("username") %> (<a href="#" class="log-out">Log out</a>)
-      </div>
-
-      <div class="section">
-
-        <header id="header">
-          <input id="new-todo" placeholder="What needs to be done?" type="text" />
-        </header>
-
-        <div id="main">
-          <input id="toggle-all" type="checkbox">
-          <label for="toggle-all">Mark all as complete</label>
-          
-          <ul id="todo-list">
-            <img src='images/spinner.gif' class='spinner' />
-          </ul>
+            <div class="photo photo10"></div>
         </div>
+    </div>
+</div>
 
-        <div id="todo-stats"></div>
-      </div>
-    </script>
+<div data-role="view" data-layout="overview-layout" id="overview-contacts" data-title="친구목록">
+    <ul data-role="listview" data-type="group">
+        <li>
+            A
+            <ul>
+                <li><h2>Ann Devon</h2><img src="../../content/mobile/overview/ann.jpg" /></li>
+                <li><h2>Annette Roulet</h2><img src="../../content/mobile/overview/annette.jpg" /></li>
+                <li><h2>Antonio Moreno</h2><img src="../../content/mobile/overview/antonio.jpg" /></li>
+            </ul>
+        </li>
+        <li>
+            C
+            <ul>
+                <li><h2>Carine Schmitt</h2><img src="../../content/mobile/overview/carine.jpg" /></li>
+                <li><h2>Catherine Dewey</h2><img src="../../content/mobile/overview/catherine.jpg" /></li>
+            </ul>
+        </li>
+        <li>
+           D
+            <ul>
+                <li><h2>Daniel Tonini</h2><img src="../../content/mobile/overview/daniel.jpg" /></li>
+                <li><h2>Diego Roel</h2><img src="../../content/mobile/overview/diego.jpg" /></li>
+            </ul>
+        </li>
+        <li>
+            E
+            <ul>
+                <li><h2>Eduardo Saavedra</h2><img src="../../content/mobile/overview/eduardo.jpg" /></li>
+                <li><h2>Elizabeth Brown</h2><img src="../../content/mobile/overview/elizabeth.jpg" /></li>
+            </ul>
+        </li>
+        <li>
+            F
+            <ul>
+                <li><h2>Felipe Izquierdo</h2><img src="../../content/mobile/overview/felipe.jpg" /></li>
+                <li><h2>Fran Wilson</h2><img src="../../content/mobile/overview/fran.jpg" /></li>
+                <li><h2>Francisco Chang</h2><img src="../../content/mobile/overview/francisco.jpg" /></li>
+            </ul>
+        </li>
+    </ul>
+</div>
 
-    <script type="text/template" id="item-template">
-      <li class="<%= done ? 'completed' : '' %>">
-	<div class="view">
-	  <input class="toggle" type="checkbox" <%= done ? 'checked="checked"' : '' %>>
-	  <label class="todo-content"><%= content %></label>
-	  <button class="todo-destroy"></button>
-	</div>
-	<input class="edit" value="<%= content %>">
-      </li>
-    </script>
+<div data-role="view" data-layout="overview-layout" data-init="mobileAccountViewInit" id="overview-account" data-title="더보기">
+    <ul id="settings-view" data-index="0">
+        <li>
+            Profile
+        </li>
+        <li>
+            General
+        </li>
+        <li>
+            Group
+        </li>
+    </ul>
+    <ul data-role="listview" data-style="inset" data-type="group">
+        <li>
+            Profile
+            <ul>
+                <li><h2>Christina <span>Berglund</span></h2><img src="../../content/mobile/overview/christina.jpg" /></li>
+                <li>Username <span class="value">christina</span></li>
+                <li>Email <span class="value">clientservice at kendoui.com</span></li>
+            </ul>
+        </li>
+        <li>
+            Languages
+            <ul>
+                <li>English <span class="value">Native</span></li>
+                <li>French<span class="value">Advanced</span></li>
+            </ul>
+        </li>
+        <li>
+            PC Skills
+            <ul>
+                <li>MS Word</li>
+                <li>MS Excel</li>
+                <li>MS Outlook</li>
+                <li>MS PowerPoint</li>
+                <li>Windows (XP, Vista)</li>
+                <li>Internet</li>
+            </ul>
+        </li>
+    </ul>
+    <ul data-role="listview" data-style="inset" data-type="group" style="display:none">
+        <li>
+            General Settings
+            <ul>
+                <li>Notifications <input type="checkbox" data-role="switch" checked></li>
+                <li>Local Services <input type="checkbox" data-role="switch"></li>
+                <li><a>About</a></li>
+            </ul>
+        </li>
+    </ul>
+    <ul data-role="listview" data-style="inset" data-type="group" style="display:none">
+        <li>
+            Group
+            <ul>
+                <li><a>Your Group Name</a></li>
+                <li><a>Group Email</a></li>
+            </ul>
+        </li>
+    </ul>
+</div>
 
-    <script type="text/template" id="stats-template">
-      <footer id="footer">
-	<span id="todo-count"><strong><%= remaining %></strong> <%= remaining == 1 ? 'item' : 'items' %> left</span>
-	<ul id="filters">
-	  <li>
-	    <a href="javascript:void(0)" id="all" class="selected">All</a>
-	  </li>
-	  <li>
-	    <a href="javascript:void(0)" id="active">Active</a>
-	  </li>
-	  <li>
-	    <a href="javascript:void(0)" id="completed">Completed</a>
-	  </li>
-	</ul>
-	<button id="clear-completed">Clear completed (<%= done %>)</button>
-      </footer>
-    </script>
 
-  </body>
 
+
+
+
+<div data-role="view" data-layout="overview-layout" id="overview-write" data-title="글쓰기" data-init="initForm">
+  <form name="myform"><input type="file" id="profilePhotoFileUpload" onclick="fileview_show();">
+    <ul data-role="listview" data-style="inset">
+      <textarea rows="7" style="width:100%; border:0px;" name="mytextfield" id="mytextfield"></textarea>
+    </ul>
+  </form>
+</div>
+
+
+
+<div data-role="layout" data-id="overview-layout">
+    <header data-role="header">
+        <div data-role="navbar">
+            <span data-role="view-title">My Contacts</span>
+            <a data-align="right" data-role="button" class="nav-button" data-icon="writeicon" href="#overview-write"></a>
+        </div>
+    </header>
+
+    <footer data-role="footer">
+        <div data-role="tabstrip">
+          <a data-icon="bookmarks" href="#overview-cities">뉴스피드
+          </a><a data-icon="home" href="#overview-gallery">내스토리
+          </a><a data-icon="contacts" href="#overview-contacts">친구목록
+          </a><a data-icon="settings" href="#overview-account">더보기</a>
+        </div>
+    </footer>
+
+</div>
+
+
+<script>
+ 
+ var write_mode = 0;
+
+  //글쓰기 완료시
+  document.addEventListener('focusout', function(e) {
+    $(".km-tabstrip").show();
+   });
+
+  function initForm() {
+    var body = $(".km-pane");
+    $(".km-tabstrip").hide();
+    write_mode = 1;
+    document.myform.mytextfield.focus();
+  }
+  
+
+  function mobileAccountViewInit() {
+      var listviews = this.element.find("ul.km-listview");
+
+      $("#settings-view").kendoMobileButtonGroup({
+          select: function() {
+              listviews.hide()
+                       .eq(this.selectedIndex)
+                       .show();
+          },
+          index: 0
+      });
+  }
+</script>
+
+<style scoped>
+.km-writeicon:after,
+.km-writeicon:before
+{
+    content: "\e07b";
+}
+
+#overview-contacts h2,
+#overview-cities h2 {
+    display: inline-block;
+    font-size: 1.1em;
+    margin: 1em 0 .5em 1em;
+}
+#overview-account h2 {
+    display: inline-block;
+    font-size: 1.1em;
+    margin: 1.5em 0 0 .6em;
+}
+#overview-account h2 span {
+    display: block;
+    clear: both;
+    font-size: 1.8em;
+    margin: .1em 0 0 0;
+}
+#overview-contacts img,
+#overview-cities img,
+#overview-account img {
+    float: left;
+    width: 4em;
+    height: 4em;
+    margin: 0;
+    -webkit-box-shadow: 0 1px 3px #333;
+    box-shadow: 0 1px 3px #333;
+    -webkit-border-radius: 8px;
+    border-radius: 8px;
+}
+
+.km-flat #overview-contacts img,
+.km-flat #overview-cities img,
+.km-flat #overview-account img {
+    -webkit-box-shadow: 0 0 0 1px rgba(0,0,0,.1);
+    box-shadow: 0 0 0 1px rgba(0,0,0,.1);
+    -webkit-border-radius: 4px;
+    border-radius: 4px;
+}
+
+#overview-account img {
+    width: 5em;
+    height: 5em;
+    margin: 1em;
+}
+#overview-account .value {
+    float: right;
+    color: #ccc;
+}
+#settings-view {
+    margin: 1em auto .4em auto;
+}
+#overview-gallery .photo {
+    width: 277px;
+    margin: 20px 20px 7px;
+    height: 220px;
+    display: inline-block;
+    -webkit-background-size: auto 100%;
+    background-size: auto 100%;
+    background-repeat: no-repeat;
+    background-position: center center;
+    -webkit-transform: translatez(0);
+}
+.km-wp #overview-gallery .photo {
+    width: 15em;
+    height: 15em;
+}
+.photo1 {background-image: url("../../content/mobile/cities/220/sydney.jpg");}
+.photo2 {background-image: url("../../content/mobile/cities/220/bonn.jpg");}
+.photo3 {background-image: url("../../content/mobile/cities/220/boston.jpg");}
+.photo4 {background-image: url("../../content/mobile/cities/220/cairo.jpg");}
+.photo5 {background-image: url("../../content/mobile/cities/220/cancun.jpg");}
+.photo6 {background-image: url("../../content/mobile/cities/220/cape-town.jpg");}
+.photo7 {background-image: url("../../content/mobile/cities/220/liverpool.jpg");}
+.photo8 {background-image: url("../../content/mobile/cities/220/london.jpg");}
+.photo9 {background-image: url("../../content/mobile/cities/220/melbourne.jpg");}
+.photo10 {background-image: url("../../content/mobile/cities/220/san-francisco.jpg");}
+
+#scrollview-container {
+    margin: 3px 0 1em 0;
+    padding-bottom: 10px;
+    background: rgba(0,0,0,.1);
+    border: 1px solid rgba(0,0,0,.3);
+    border-width: 1px 0;
+    box-shadow: 0 1px 1px rgba(255,255,255,.3);
+}
+.km-flat #scrollview-container {
+    background: transparent;
+    border: 0;
+    box-shadow: none;
+}
+#overview-gallery .km-ios .km-content {
+    background: #bbb;
+}
+
+</style>
+
+
+<script>
+  document.title = "SSKY STORY";
+
+  window.kendoMobileApplication = new kendo.mobile.Application($(document.body), {
+      skin : "flat",
+      layout: "examples",
+      transition: "slide",
+      icon: {
+          "" : '/favicon.png',
+          "72x72" : '/favicon.png',
+          "114x114" : '/favicon114.png'
+      }
+  });
+</script>
+
+
+</body>
 </html>
